b1fac231dfb0640102436e5ae055d89df77f63e8
jzq2000 <juzeqian@mail.ustc.edu.cn>
2020-05-05 15:32:25 +0800

    joystick demo

--- assets/Script/minimap.js
+++ assets/Script/minimap.js
@@ -11,11 +11,11 @@ cc.Class({
   properties: {
     background: {
       default: null,
-      type: cc.Sprite,
+      type: cc.Node,
     },
     minimap: {
       default: null,
-      type: cc.Sprite,
+      type: cc.Node,
     },
     maincamera: {
       default: null,
@@ -38,51 +38,52 @@ cc.Class({
   },
   mapToMini(point) {
     const x =
-      (point.x / this.background.node.width) *
-      this.minimap.node.width *
-      this.minimap.node.scaleX;
+      (point.x / this.background.width) *
+      this.minimap.width *
+      this.minimap.scaleX;
     const y =
-      (point.y / this.background.node.height) *
-      this.minimap.node.height *
-      this.minimap.node.scaleY;
+      (point.y / this.background.height) *
+      this.minimap.height *
+      this.minimap.scaleY;
     return cc.v2(x, y);
   },
   miniToMap(point) {
     const x =
-      ((point.x / this.minimap.node.width) * this.background.node.width) /
-      this.minimap.node.scaleX;
+      ((point.x / this.minimap.width) * this.background.width) /
+      this.minimap.scaleX;
     const y =
-      ((point.y / this.minimap.node.height) * this.background.node.height) /
-      this.minimap.node.scaleY;
+      ((point.y / this.minimap.height) * this.background.height) /
+      this.minimap.scaleY;
     return cc.v2(x, y);
   },
   onLoad() {
-    this.minimap.node.on('touchstart', this.onTouchStart, this);
-    this.minimap.node.on('touchmove', this.onTouchMove, this);
-    this.minimap.node.on('touchend', this.onTouchEnd, this);
-    this.minimap.node.on('touchcancel', this.onTouchEnd, this);
+    this.minimap.on('touchstart', this.onTouchStart, this);
+    this.minimap.on('touchmove', this.onTouchMove, this);
+    this.minimap.on('touchend', this.onTouchEnd, this);
+    this.minimap.on('touchcancel', this.onTouchEnd, this);
   },
   start() {
     //小地图
     this.updateCameraPosition(cc.v2(this.player.x, this.player.y));
     let texture = new cc.RenderTexture();
-    texture.initWithSize(this.minimap.node.width, this.minimap.node.height);
+    texture.initWithSize(this.minimap.width, this.minimap.height);
     let spriteFrame = new cc.SpriteFrame();
+    let sprite = this.minimap.getComponent(cc.Sprite);
     spriteFrame.setTexture(texture);
-    this.minimap.spriteFrame = spriteFrame;
+    sprite.spriteFrame = spriteFrame;
     this.camera.targetTexture = texture;
     //this.minimap.node.tween(cc.follow(this.player));
   },
   onDestroy() {
-    this.minimap.node.off('touchstart', this.onTouchStart, this);
-    this.minimap.node.off('touchmove', this.onTouchMove, this);
-    this.minimap.node.off('touchend', this.onTouchEnd, this);
-    this.minimap.node.off('touchcancel', this.onTouchEnd, this);
+    this.minimap.off('touchstart', this.onTouchStart, this);
+    this.minimap.off('touchmove', this.onTouchMove, this);
+    this.minimap.off('touchend', this.onTouchEnd, this);
+    this.minimap.off('touchcancel', this.onTouchEnd, this);
   },
   touchMiniMap(event) {
     // console.log(event.getLocation().x);
     // console.log(event.getLocation().y);
-    const touchLocation = this.minimap.node.convertToNodeSpaceAR(
+    const touchLocation = this.minimap.convertToNodeSpaceAR(
       event.getLocation()
     );
     // console.log(touchLocation.x);
@@ -124,7 +125,6 @@ cc.Class({
     // );
     let pointCamera = this.maincamera.node.position;
     pointCamera = this.mapToMini(pointCamera);
-    console.log(pointCamera);
     this.rect.position = pointCamera;
     let canvas = this.rect.getComponent(cc.Graphics);
     let rect_size = this.rect.getContentSize();

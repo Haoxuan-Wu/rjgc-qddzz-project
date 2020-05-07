8ed0f1b2f9db36a1c414dcad72db05e2f410a21f
ZhangMZh <1441033251@qq.com>
2020-05-07 11:23:59 +0800

    Add Graphic function

--- assets/Script/Grid.js
+++ assets/Script/Grid.js
@@ -1,25 +1,47 @@
-// Learn cc.Class:
-//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
   extends: cc.Component,
 
   properties: {
+    graphNode: {
+      default: null,
+      type: cc.Node,
+    },
     player: {
       default: null,
       type: cc.Node,
+    },
+    gridPixel: {
+      default: 30,
     },
   },
 
   onLoad() {},
 
   start() {
-    cc.log(this.player.getPosition());
+    cc.log(this.convertToGridX(this.player.getPosition().x));
+    cc.log();
   },
 
-  update(dt) {},
+  update(dt) {
+    let gridX =
+      this.convertToGridX(this.player.getPosition().x) * this.gridPixel;
+    let gridY =
+      this.convertToGridY(this.player.getPosition().y) * this.gridPixel;
+    this.DyeGrid(gridX, gridY);
+  },
+
+  // 将小羊坐标转换为网格坐标，以左下角为坐标原点
+  convertToGridX: function (x) {
+    return parseInt((x + 1050) / this.gridPixel);
+  },
+  convertToGridY: function (y) {
+    return parseInt((y + 750) / this.gridPixel);
+  },
+  // 将网格染色
+  DyeGrid: function (gridX, gridY) {
+    let graphics = this.graphNode.getComponent(cc.Graphics);
+    graphics.rect(gridX, gridY, this.gridPixel, this.gridPixel);
+    graphics.fillColor = cc.Color.RED;
+    graphics.fill();
+  },
 });

2896e0de4638577b22dd3df2328d87273c56466a
ZhangMZh <1441033251@qq.com>
2020-05-08 11:18:20 +0800

    Add grid array

--- assets/Script/Grid.js
+++ assets/Script/Grid.js
@@ -13,21 +13,30 @@ cc.Class({
     gridPixel: {
       default: 30,
     },
+    gridColor: [],
   },
 
-  onLoad() {},
-
-  start() {
-    cc.log(this.convertToGridX(this.player.getPosition().x));
-    cc.log();
+  onLoad() {
+    this.initColor();
   },
+  // 初始化记录方格颜色的数组
+  initColor: function () {
+    for (let i = 0; i < 70; i++) {
+      this.gridColor[i] = [];
+      for (let j = 0; j < 50; j++) {
+        this.gridColor[i][j] = 0;
+      }
+    }
+  },
+
+  start() {},
 
   update(dt) {
-    let gridX =
-      this.convertToGridX(this.player.getPosition().x) * this.gridPixel;
-    let gridY =
-      this.convertToGridY(this.player.getPosition().y) * this.gridPixel;
-    this.DyeGrid(gridX, gridY);
+    let gridX = this.convertToGridX(this.player.getPosition().x);
+    let gridY = this.convertToGridY(this.player.getPosition().y);
+    if (this.gridColor[gridX][gridY] == 0) {
+      this.DyeGrid(gridX, gridY);
+    }
   },
 
   // 将小羊坐标转换为网格坐标，以左下角为坐标原点
@@ -40,8 +49,13 @@ cc.Class({
   // 将网格染色
   DyeGrid: function (gridX, gridY) {
     let graphics = this.graphNode.getComponent(cc.Graphics);
-    graphics.rect(gridX + 1, gridY + 1, this.gridPixel - 2, this.gridPixel - 2);
     graphics.fillColor = cc.Color.RED;
-    graphics.fill();
+    graphics.fillRect(
+      gridX * this.gridPixel + 1,
+      gridY * this.gridPixel + 1,
+      this.gridPixel - 2,
+      this.gridPixel - 2
+    );
+    this.gridColor[gridX][gridY] = 1;
   },
 });
